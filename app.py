18f690ca9a9f509f08f302bdec5d7c3704777c65
sungwookoo <sungwookoo.dev@gmail.com>
2022-05-04 16:06:45 +0900

    feat : 로그인 jwt 토큰 생성 기능 추가

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, render_template, jsonify, request, redirect
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from pymongo import MongoClient
 import jwt
 import hashlib
@@ -16,19 +16,27 @@ db = client.dbinsta
 # client = config.DbConfig().client
 # db = client.dbinsta
 
-
 # JWT
 SECRET_KEY = config.Config.SECRET_KEY
 
 
 @app.route('/')
-def index():
-    return redirect('/login')
+def home():
+    token_receive = request.cookies.get('token')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user = db.users.find_one({'user_id': payload['id']})
+        return render_template('main.html', user_id=user["user_id"])
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for('login', msg='로그인이 만료되었습니다.'))
+    except jwt.exceptions.DecodeError:
+        return render_template('login.html')
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    msg = request.args.get('msg')
+    return render_template('login.html', msg=msg)
 
 
 @app.route('/signup')
@@ -36,7 +44,7 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/api/register', methods=['POST'])
 def sign_up():
     password = request.form['pw_give']
     hashed_pw = hashlib.sha256(password.encode('utf-8')).hexdigest()
@@ -45,42 +53,34 @@ def sign_up():
            'password': hashed_pw,
            'name': request.form['name_give'],
            'phone': request.form['phone_give'],
-           'email': request.form['email_give']
+           'email': request.form['email_give'],
+           'profile_img_src': ''
            }
     db.users.insert_one(doc)
 
     return jsonify({'msg': '회원가입이 완료되었습니다.'})
 
 
-@app.route("/login", methods=['POST'])
+# 로그인 API
+@app.route("/api/login", methods=['POST'])
 def login_proc():
     user_id = request.form['id_give']
     password = request.form['pw_give']
     hashed_pw = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
-    payload = {'id': user_id,
-               'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=1800)},
-    user = db.users.find_one({'user_id': user_id})
+    user = db.user.find_one({'id': user_id, 'pw': hashed_pw})
 
-    # 없는 ID
     if user is None:
-        return jsonify({
-            'result': 'none'
-        })
+        payload = {'id': user_id,
+                   'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60)}
 
-    # 아이디, 암호 일치
-    if user_id == user['user_id'] and password == hashed_pw:
         return jsonify({
             'result': 'success',
             # 검증된 경우, access 토큰 반환
-            'token': jwt.encode(payload, app.config['JWT_SECRET_KEY'], algorithm='HS256')
+            'token': jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         })
-
-    # 아이디, 암호 불일치
     else:
-        return jsonify({
-            'result': 'wrong'
-        })
+        return jsonify({'result': 'fail', 'msg': '아이디 또는 비밀번호가 일치하지 않습니다.'})
 
 
 @app.route('/main')
