d413d49523c1fe813de84279385e10f6cbac5a80
gyuhyeongH <ckacl792@gmail.com>
2022-05-05 15:19:00 +0900

    feat : 테스트 데이터로 피드 추가 기능 추가(post,get api코드 )

--- app.py
+++ app.py
@@ -159,6 +159,40 @@ def save_comment():
     return jsonify({'msg': '댓글이 작성되었습니다.'})
 
 
+# 피드 작성(POST) API
+@app.route('/api/feed', methods=['POST'])
+def save_feed():
+    user_id_receive = request.form['user_id']
+    created_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
+    doc = {
+        'user_id': user_id_receive,
+        'created_at': created_at
+    }
+
+    db.comments.insert_one(doc)
+
+    return jsonify({'msg': '작성되었습니다.'})
+
+# id를 문자열로 바꾸는 함수
+def objectIdToString(find_list):
+    results = []
+    for i in find_list:
+        i['_id'] = str(i['_id'])
+        results.append(i)
+    return results
+
+@app.route('/api/feed', methods=['GET'])
+def get_feed():
+    users = list(db.users.find({}))
+    feeds = list(db.feed.find({}))
+    feeds = objectIdToString(feeds)
+    users = objectIdToString(users)
+    return jsonify({'all_users': users,
+                    'all_feeds': feeds
+                    })
+
+
 # 댓글 작성(POST) API
 @app.route('/api/comment', methods=['POST'])
 def save_comment():
