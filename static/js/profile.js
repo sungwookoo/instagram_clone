859c4d1dd28467c012540664987b09fc6b19cf8f
HWISU96 <soshyzx123@naver.com>
2022-05-10 14:43:46 +0900

    feat : 프로필 feed 추가

--- static/js/profile.js
+++ static/js/profile.js
@@ -29,48 +29,44 @@ function getProfile() {
             console.log(response)
 
             let users = response['all_users'];
+            let feeds = response['all_feeds'];
             // let followers = response['all_followers'];
             // let followings = response['all_followings'];
+
             for (let i = 0; i < users.length; i++) {
                 if (current_user_id === users[i]['user_id']) {
                     let profile_img = users[i]['profile_img_src'];
                     let name = users[i]['name'];
                     let user_id = users[i]['user_id'];
                     let temp_myprofile = `
-                            <div class="profile_img">
-                            <img class="profile_pic" src="${profile_img}" alt="프로필"></div>
-                            <div class="profile_word">
-                                <div class="profile_nickname_box">
-                                    <div class="profile_nickname">${user_id}</div>
-                                    <div class="profile_setting">
-                                        <button class="setting_button">프로필 편집</button>
-                                    </div>
+                        <div class="profile_img">
+                        <img class="profile_pic" src="${profile_img}" alt="프로필"></div>
+                        <div class="profile_word">
+                            <div class="profile_nickname_box">
+                                <div class="profile_nickname">${user_id}</div>
+                                <div class="profile_setting">
+                                    <button class="setting_button">프로필 편집</button>
                                 </div>
-                                <div class="profile_post_box">
-                                    <div class="profile_post">게시물{feed_count}</div>
-                                    <div class="profile_follower">팔로워{temp}</div>
-                                    <div class="profile_follow">팔로우{temp}</div>
-                                </div>
-                                <div class="profile_name"><strong>${name}</strong></div>
                             </div>
+                            <div class="profile_post_box">
+                                <div class="profile_post">게시물{feed_count}</div>
+                                <div class="profile_follower">팔로워{temp}</div>
+                                <div class="profile_follow">팔로우{temp}</div>
+                            </div>
+                            <div class="profile_name"><strong>${name}</strong></div>
+                        </div>
                     `
                     $('#myprofileimg').append(temp_myprofile);
-
-
-                    // let feeds = response['all_feeds'];
-                    // for (let i = 0; i < feeds.length; i++) {
-                    //     let user_id = feeds[i]['user_id'];
-                    //     let feed_img_src = feeds[i]['feed_img_src'];
-                    //
-                    //     for (let k = 0; k < feeds.length; k++) {
-                    //         if (current_user_id === feeds[j]['user_id']) {
-                    //             let feed_img_src = feeds[j]['feed_img_src'];
-                    //             let temp_html = `
-                    //
-                    //             `
-                    //         }
-                    //     }
-                    // }
+                }
+            }
+            for (let k = 0; k < feeds.length; k++) {
+                if (current_user_id === feeds[k]['user_id']) {
+                    let feed_img_src = feeds[k]['feed_img_src'];
+                    let temp_myfeed = `
+                        <div class="feed"><img src="${feed_img_src}" width="300" height="300"></div>
+                        </div>
+                        `
+                    $('#myfeedimg').append(temp_myfeed);
                 }
             }
         }
